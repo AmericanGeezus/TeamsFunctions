7fe625920bdbe3177ac8e72f4429df297e813746
DEberhardt <david.eberhardt@outlook.com>
2020-10-06 16:14:36 +0100

    Tests

--- TeamsFunctions.Tests.ps1
+++ TeamsFunctions.Tests.ps1
@@ -1,17 +1,27 @@
 ﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$module = 'TeamsFunctions'
+$name = 'TeamsFunctions'
 
-Describe -Tags ('Unit', 'Acceptance') "$module Module Tests"  {
+BeforeAll {
+  $module = @{
+    Name     = $name
+    Path     = $here
+    Manifest = "$here\$name.psd1"
+    FileName = "$here\$name.psm1"
+  }
+  Write-Host "Testing $($Module.name) in path $($module.path)"
+}
+
+Describe -Tags ('Unit', 'Acceptance') "$($module.Name) Module Tests"  {
 
   Context 'Module Setup' {
-    It "has the root module $module.psm1" {
-      "$here\$module.psm1" | Should -Exist
+    It "has the root module '$name.psm1'" {
+      "$($module.FileName)" | Should -Exist
     }
 
     It "has the a manifest file of $module.psd1" {
-      "$here\$module.psd1" | Should -Exist
-      "$here\$module.psd1" | Should -FileContentMatch "$module.psm1"
-    }
+      "$($module.Manifest)" | Should -Exist
+      "$($module.Manifest)" | Should -FileContentMatch "$($module.FileName)"
+    } -TestCases { module = $module}
 
     It "$module folder has functions" {
       "$here\Public\Functions" | Should -Exist
@@ -27,84 +37,77 @@ Describe -Tags ('Unit', 'Acceptance') "$module Module Tests"  {
 
   } # Context 'Module Setup'
 
-  $functions = Get-ChildItem "$here\Public", "$here\Private" -Include "*.ps1" -ExClude "*.Tests.ps1" -Recurse #| Select-Object -First 1
 
-  foreach ($function in $functions) {
-    BeforeEach {
-        $hash = @{
-            Name = $function.BaseName
-            Path = $function.FullName
-        }
+  $functions = Get-ChildItem "$here\Public", "$here\Private" -Include "*.ps1" -Exclude "*.Tests.ps1" -Recurse | Select-Object -First 1
+
+  Context "Functions" -ForEach $functions {
+    It "$_ should exist" {
+      Write-Host "->$($_.FullName)<-"
+      "$($_.FullName)" | Should -Exist
     }
 
-    Context "$($hash.Name) - Function" {
+    <#
+    It "should have a valid header" {
+      Should "$($function.FullName)" -FileContentMatch 'Module:'
+      Should "$($function.FullName)" -FileContentMatch 'Function:'
+      Should "$($function.FullName)" -FileContentMatch 'Author:'
+      Should "$($function.FullName)" -FileContentMatch 'Updated:'
+      Should "$($function.FullName)" -FileContentMatch 'Status:'
+    }
 
-      It "should exist" {
-        Should "$($function.FullName)" -Exist
-      }
+    It "should have help block" {
+      Should "$($function.FullName)" -FileContentMatch "<#"
+      Should "$($function.FullName)" -FileContentMatch "# >"
+    }
 
-      It "should have a valid header" {
-        Should "$($function.FullName)" -FileContentMatch 'Module:'
-        Should "$($function.FullName)" -FileContentMatch 'Function:'
-        Should "$($function.FullName)" -FileContentMatch 'Author:'
-        Should "$($function.FullName)" -FileContentMatch 'Updated:'
-        Should "$($function.FullName)" -FileContentMatch 'Status:'
-      }
+    It "should have a SYNOPSIS section in the help block" {
+      Should "$($function.FullName)" -FileContentMatch '.SYNOPSIS'
+    }
 
-      It "should have help block" {
-        Should "$($function.FullName)" -FileContentMatch "<#"
-        Should "$($function.FullName)" -FileContentMatch "#>"
-      }
+    It "should have a DESCRIPTION section in the help block" {
+      Should "$($function.FullName)" -FileContentMatch '.DESCRIPTION'
+    }
 
-      It "should have a SYNOPSIS section in the help block" {
-        Should "$($function.FullName)" -FileContentMatch '.SYNOPSIS'
-      }
+    It "should have a EXAMPLE section in the help block" {
+      Should "$($function.FullName)" -FileContentMatch '.EXAMPLE'
+    }
 
-      It "should have a DESCRIPTION section in the help block" {
-        Should "$($function.FullName)" -FileContentMatch '.DESCRIPTION'
-      }
+    # Add more checks for !
 
-      It "should have a EXAMPLE section in the help block" {
-        Should "$($function.FullName)" -FileContentMatch '.EXAMPLE'
-      }
+    # Evaluate use - not all Functions are advanced yet!
 
-      # Add more checks for !
+    It "should be an advanced function" {
+      Should "$($function.FullName)" -FileContentMatch 'function'
+      Should "$($function.FullName)" -FileContentMatch 'cmdletbinding'
+      Should "$($function.FullName)" -FileContentMatch 'param'
+    }
 
-      # Evaluate use - not all Functions are advanced yet!
+    It "should have an OutputType" {
+      Should "$($function.FullName)" -FileContentMatch '[OutputType(*)]'
+    }
 
-      It "should be an advanced function" {
-        Should "$($function.FullName)" -FileContentMatch 'function'
-        Should "$($function.FullName)" -FileContentMatch 'cmdletbinding'
-        Should "$($function.FullName)" -FileContentMatch 'param'
-      }
+    It "should contain Write-Verbose blocks" {
+      Should "$($function.FullName)" FileContentMatch 'Write-Verbose'
+    }
 
-      It "should have an OutputType" {
-        Should "$($function.FullName)" -FileContentMatch '[OutputType(*)]'
-      }
-
-      It "should contain Write-Verbose blocks" {
-        Should "$($function.FullName)" FileContentMatch 'Write-Verbose'
-      }
-
+    It "is valid PowerShell code" {
       $psFile = Get-Content -Path $function.FullName -ErrorAction Stop
       $errors = $null
       $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
-      It "is valid PowerShell code" {
-        $errors.Count | Should -Be 0
-      }
-
-
-    } # Context "Test Function $function"
-
-    <# Commenting out as there aren't any tests files for individual files yet.
-    Context "$function has tests" {
-      $functionTests = (Get-ChildItem "$Function.Tests.ps1" -Recurse).FullName
-
-      It "$($function).Tests.ps1 should exist" {
-        $functionTests | Should -Exist
-      }
+      $errors.Count | Should -Be 0
     }
-    #>
-  } # foreach ($function in $functions)
+#>
+
+  } # Context "Test Function $function"
+
+  <# Commenting out as there aren't any tests files for individual files yet.
+  Context "$function has tests" {
+    $functionTests = (Get-ChildItem "$Function.Tests.ps1" -Recurse).FullName
+
+    It "$($function).Tests.ps1 should exist" {
+      $functionTests | Should -Exist
+    }
+  }
+  #>
 
 }
