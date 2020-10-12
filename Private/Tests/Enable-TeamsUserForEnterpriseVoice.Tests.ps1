7118ee835c4ffe8d96b6c8e62f989bdf8c30ba08
David Eberhardt <David.Eberhardt@outlook.com>
2020-10-12 13:51:19 +0100

    Tests EV

--- Private/Tests/Enable-TeamsUserForEnterpriseVoice.Tests.ps1
+++ Private/Tests/Enable-TeamsUserForEnterpriseVoice.Tests.ps1
@@ -1,3 +1,20 @@
-﻿
-$here = Get-Location
-$loc = $here.ToString().replace("Tests", "Functions")
+﻿# Module:   TeamsFunctions
+# Function: Test
+# Author:		David Eberhardt
+# Updated:  11-OCT-2020
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$here
+$target = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\\Tests\\', '\\Functions\\' -replace '\.Tests\.', '.'
+$target
+$Function = $target -Replace '.ps1.', ''
+$Function
+#. "$here\$target"
+
+Describe -Tags ('Unit', 'Acceptance') "$Function" {
+
+  It 'Calls with no switch parameter set' {
+    { "$Function" } | Should -BeFalse
+
+  }
+}
